b8939160e6a3e0b98c5ca70225cb259b4b62df63
ARBhosale <anirsk@gmail.com>
2020-10-02 11:34:49 -0700

    adding attr/props and styles to game-message

--- src/components/lit/game-message.js
+++ src/components/lit/game-message.js
@@ -1,8 +1,40 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, html, css } from 'lit-element';
+import { constants } from '../../constants';
 
 class GameMessage extends LitElement {
+
+    constructor() {
+        super();
+        this.bold = this.bold != undefined;
+        this.big = this.big != undefined;
+    }
+
+    static get properties() {
+        return {
+            text: { attribute: true },
+            bold: { attribute: true, },
+            big: { attribute: true, },
+        };
+    }
+
+    static get styles() {
+        return css`
+        p {
+            color: ${constants.FONT_COLOR};
+        }`;
+    }
+
+    get styles() {
+        let weight = `font-weight: ${this.bold ? css`bold` : css`normal`};`;
+        let size = `font-size: ${this.big ? css`${constants.FONT_SIZE_BIG}` : css`inherit;`};`;
+        return `${weight}${size}`;
+    }
+
     render() {
-        return html`<div>Hello Message</div>`;
+        return html`
+        <p style="${this.styles}">
+            ${this.text}
+        </p>`;
     }
 }
 
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+import { css } from 'lit-element';
+export const constants = {
+    FONT_COLOR: css`#2c3e50`,
+    FONT_SIZE_BIG: css`24px`,
+}
